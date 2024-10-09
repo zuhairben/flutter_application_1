081af2f3c1b3581f309b6d6aa60aa8ebe36fa8b1
zuhairben <zuhairshah02@gmail.com>
2024-10-09 11:53:17 +0500

    state management

--- lib/ListElement.dart
+++ lib/ListElement.dart
@@ -1,82 +1,101 @@
+
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class ListElement extends StatefulWidget {
-  const ListElement({ super.key });
+class NewList extends StatefulWidget{
 
-  
+  const NewList({super.key});
 
   @override
-  State<ListElement> createState() => _ListElementState();
+  State<NewList> createState() => _NewListState();
 }
 
-class _ListElementState extends State<ListElement> {
-  Future<List<Post>> fetchAllPosts() async {
-    final response = await http.get(Uri.parse('https://jsonplaceholder.org/posts'));
-    
+class Post{
+
+
+ final int id;
+ final String slug;
+ final String url;
+ final String title;
+ final String content;
+ final String image;
+ // final String thumbnail;
+ // final String status;
+ // final DateTime publishedAt;
+ // final DateTime updatedAt;
+ // final int userId;
+
+ Post({
+   required this.id,
+   required this.slug,
+   required this.url,
+   required this.title,
+   required this.content,
+   required this.image
+ });
+
+
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      slug: json['slug'],
+      url: json['url'],
+      title: json['title'],
+      content: json['content'],
+      image: json['image'],
+    );
+  }
+}
+
+class _NewListState extends State<NewList>{
+
+  List<Post> _posts = [];
+  bool _isLoading = true;
+
+
+  Future<void> fetchAllPosts() async{
+    setState(() {_isLoading = true;});
+    try{
+    final response = await http.get(Uri.parse("https://jsonplaceholder.org/posts"));
     if(response.statusCode == 200){
       List jsonResponse = jsonDecode(response.body);
-      return jsonResponse.map((post) => Post.fromJson(post)).toList();
+      _posts = jsonResponse.map((post) => Post.fromJson(post)).toList();
     }
-    else{
-      throw Exception('Failed to load posts');
+    else throw Exception("Failed to load posts");
     }
+    catch(exp){
+      throw Exception("Failed to load posts");
+    }
+    finally{
+    setState(() {_isLoading = false;});
+  }}
+
+  @override
+  void initState(){
+    fetchAllPosts();
+    super.initState();
   }
+
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: FutureBuilder(future: fetchAllPosts(), 
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            return ListView.builder(itemBuilder: (context, index){
-              return Card(
-                key: ValueKey(index),
-                child: ListTile(
-                  leading: Image.network(snapshot.data![index].image),
-                  title: Text(snapshot.data![index].title),
-                  subtitle: Text(snapshot.data![index].content, overflow: TextOverflow.ellipsis,),
-                ),
-              );
-            });
-          }
-          else if(snapshot.hasError){
-            return Text('error fetch');
-          }
-          return Center(child: CircularProgressIndicator());
-        }),
-      );
+
+
+    return Scaffold(
+        body: Center(
+          child:_isLoading ? CircularProgressIndicator() : ListView.builder(itemBuilder: (c, i){
+            var _item = _posts[i];
+            return ListTile(title: Text(_item.title), subtitle: Text(_item.content, overflow: TextOverflow.ellipsis,),
+            leading: CircleAvatar(child: Image.network(_item.image),),);
+          }),
+
+
+    ));
   }
+
+
 }
-
-
-
-class Post{
-  final int id;
-  final String slug;
-  final String url;
-  final String title;
-  final String content;
-  final String image;
-
-  Post({
-  required this.id,
-  required this.slug,
-  required this.url,
-  required this.title,
-  required this.content,
-  required this.image
-
-  });
-
-  factory Post.fromJson(Map<String, dynamic> json){
-    return Post(id: json['id'], slug: json['slug'], url: json['url'], title: json['title'], content: json['content'], image: json['image']);
-  }
-}
-
-
-
-
-
